a712ef8c06754ecaf0ef66ab2b2083823971cfaa
navpreet <navpreetbatth2.nb@gmail.com>
2024-01-18 09:05:01 -0800

    initial commit

--- VariablesExercise/Program.cs
+++ VariablesExercise/Program.cs
@@ -4,7 +4,18 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            // Declare and initialize variables
+            string myString = "C#";
+            int myInt = 42;
+            char myChar = 'A';
+            bool myBool = true;
+            double myDouble = 3.14;
+            decimal myDecimal = 123.45M;
+
+            // Interpolate variables in a Console.WriteLine();
+            Console.WriteLine($"My string is: {myString}. My integer is {myInt}. My nickename is {myChar}. I am {myBool} to myself. Math fact is - the pie value is {myDouble} and decimal value can be written as {myDecimal}");
+            
+            
         }
     }
 }
